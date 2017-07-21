3efa867dd533c5ed01b56651e23e12935098208d
Andrey Filchenkov <afi@sitecore.net>
2017-07-21 16:26:02 +0300

    Init files

---
+++ src/Sitecore.Support.436732/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.436732")]
+[assembly: AssemblyProduct("Sitecore.Support.436732")]
+[assembly: ComVisible(false)]
